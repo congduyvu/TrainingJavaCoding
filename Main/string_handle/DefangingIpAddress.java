63b45990ee508a391fa71242af96767d091bf447
kunsc <congduyvu1997@gmail.com>
2024-09-01 17:58:50 +0700

    Task/2

---
+++ Main/string_handle/DefangingIpAddress.java
@@ -0,0 +1,19 @@
+package string_handle;
+
+public class DefangingIpAddress {
+
+    public static void main(String[] args) {
+
+        String ipAddress1 = "1.1.1.1";
+        String result1 = convertIpAddress(ipAddress1);
+        System.out.println(result1);
+
+        String ipAddress2 = "255.100.50.0";
+        String result2 = convertIpAddress(ipAddress2);
+        System.out.println(result2);
+    }
+
+    public static String convertIpAddress(String ipAddress){
+        return ipAddress.replace(".", "[.]");
+    }
+}

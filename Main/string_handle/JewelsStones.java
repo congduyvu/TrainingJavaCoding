034111a084b1db343a252a1d3fa95800dc33c79a
kunsc <congduyvu1997@gmail.com>
2024-09-01 20:19:11 +0700

    Task/4

---
+++ Main/string_handle/JewelsStones.java
@@ -0,0 +1,31 @@
+package string_handle;
+
+public class JewelsStones {
+
+    public static void main(String[] args) {
+
+        String jewels1 = "aA";
+        String stones1 = "aAAbbbb";
+        int result1 = countJewelsInStones(jewels1, stones1);
+        System.out.println(result1);
+
+        String jewels2 = "z";
+        String stones2 = "ZZ";
+        int result2 = countJewelsInStones(jewels2, stones2);
+        System.out.println(result2);
+    }
+
+    public static Integer countJewelsInStones(String jewels, String stones){
+        int result = 0;
+        for (int i = 0; i < jewels.length(); i++){
+            char key = jewels.charAt(i);
+            for (int j = 0; j < stones.length(); j++){
+                char elementInStone = stones.charAt(j);
+                if(String.valueOf(key).equals(String.valueOf(elementInStone))){
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+}
